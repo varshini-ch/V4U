47f29adf46552e3b6c1279f75816640aecd5c967
varshini-ch <2varshi2020@gmail.com>
2025-10-16 15:59:23 +0530

    Add database connection script in conn.php

---
+++ public/conn.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'mysql.railway.internal'; // Replace with your Railway host
+$dbname = 'railway';                          // Replace with your Railway database name
+$user = 'root';
+$password = 'YGFPuRaPUubtLxRQXvqONNWsVgGPpzQp';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    // Set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
